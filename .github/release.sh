52de89155c3b1e5b02615ecf42db8552cda718b2
D-Nice <d-nice2@protonmail.com>
2020-02-19 16:58:45 -0500

    chore: update release script
    allows for any additional args to be passed by user when calling it, which is passed off to standard-version.

--- .github/release.sh
+++ .github/release.sh
@@ -3,7 +3,10 @@ FIRST=""
 if [ ! -f CHANGELOG.md ]; then
   FIRST="-f"
 fi
+
+# supports passing of additional args to standard-version
 npx standard-version@7.1.0 "$FIRST" --header '# CHANGELOG
 ' \
+  "$@" \
   && echo "Sign-off commit and tags via 'git commit --amend --signoff' and 'git tag -sf v...'" \
   && echo "Appropriately bump nimble file version as well in the amend..."

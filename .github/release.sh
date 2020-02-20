577f0c58a382b65553142a74ea9d59e079359180
D-Nice <d-nice2@protonmail.com>
2020-02-20 18:02:45 -0500

    chore: cleanup release script
    it seems to also have a general issue doing a first release with package file potentially?? Essentially freezes on the tagging part...

--- .github/release.sh
+++ .github/release.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 FIRST=""
 if [ ! -f CHANGELOG.md ]; then
-  #VERSION=$(find ./ -name .v.semver -exec cat {} \; | head -n1 | tr -d '\n')
   FIRST="-f"
 fi
 
 # supports passing of additional args to standard-version
-npx standard-version@7.1.0 "$FIRST" --header '# CHANGELOG
+npx standard-version@7.1.0 "$FIRST" "$@" --header '# CHANGELOG
 ' \
-  "$@" \
   && echo "Sign-off commit and tags via 'git commit --amend --signoff' and 'git tag -sf v...'"
